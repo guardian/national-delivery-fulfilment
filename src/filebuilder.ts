038700cca66c38cb8110c37340e661046f384916
Pascal Honoré <pascal@alseyn.net>
2023-08-25 16:54:01 +0100

    testing: mixing import styles for csv-writer

--- src/filebuilder.ts
+++ src/filebuilder.ts
@@ -1,6 +1,13 @@
-import * as csv from 'fast-csv'
 import moment from 'moment';
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 export function make1() {
+  const csvWriter = createCsvWriter({
+    path: 'path/to/file.csv',
+    header: [
+        {id: 'name', title: 'NAME'},
+        {id: 'lang', title: 'LANGUAGE'}
+    ]
+  });
   return moment().format('MMMM Do YYYY, h:mm:ss a');
 }
