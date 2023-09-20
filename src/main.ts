3adae33aaa2299b59836f84453bef9a68ed6375e
Pascal Honoré <pascal@alseyn.net>
2023-09-20 10:27:43 +0100

    logging/debug (2)

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ export const main = async () => {
       //const file2 = fileRecordsToCSVFile(fileRecords);
       //const filePathKey = `fulfilment/${cursor.format("YYYY")}/${cursor.format("YYYY-MM")}/${cursor.format("YYYY-MM-DD")}.csv`;
       //await commitFileToS3_v3(Stage, filePathKey, file2);
-      //await sleep(2000); // sleeping 2 seconds
+      await sleep(2000); // sleeping 2 seconds
     }
   } else {
     console.log("Could not extract a bearer token from zuora")
