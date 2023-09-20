b113bb73123d7f1b05b62ea481a7c34c279c26cd
Pascal Honoré <pascal@alseyn.net>
2023-09-20 10:46:40 +0100

    logging/debug (4)

--- src/main.ts
+++ src/main.ts
@@ -15,16 +15,17 @@ export const main = async () => {
   if (zuoraBearerToken) {
     for (const i of Array(14).keys()) {
       console.log(`i: ${i}`);
-      //const cursor = moment().add(i, "days");
-      //const date = cursor.format("YYYY-MM-DD");
-      //const file1 = await cycleDataFileFromZuora(Stage, zuoraBearerToken);
+      console.log(`timestamp: ${new Date()}`);
+      const cursor = moment().add(i, "days");
+      const date = cursor.format("YYYY-MM-DD");
+      console.log(`timestamp: ${date}`);
+      const file1 = await cycleDataFileFromZuora(Stage, zuoraBearerToken);
       //const subscriptions = zuoraDataFileToSubscriptions(file1);
       //const fileRecords = subscriptionsToFileRecords(subscriptions);
       //const file2 = fileRecordsToCSVFile(fileRecords);
       //const filePathKey = `fulfilment/${cursor.format("YYYY")}/${cursor.format("YYYY-MM")}/${cursor.format("YYYY-MM-DD")}.csv`;
       //await commitFileToS3_v3(Stage, filePathKey, file2);
       await sleep(2000); // sleeping 2 seconds
-      console.log(`timestamp: ${new Date()}`);
     }
   } else {
     console.log("Could not extract a bearer token from zuora")
