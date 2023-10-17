0e232cb53bd55f365e4dd04777fb5e49e5cef8fb
Pascal Honoré <pascal@alseyn.net>
2023-10-17 17:24:07 +0100

    renaming and refactoring

--- src/main.ts
+++ src/main.ts
@@ -21,14 +21,14 @@ export const main = async () => {
       const date = cursor.format("YYYY-MM-DD");
       console.log(`i: ${i}; date: ${date}`);
       const zuoraDataFiles = await cycleDataFilesFromZuora(Stage, zuoraBearerToken, date);
-      const subscriptions1a = subscriptionsDataFileToSubscriptions(zuoraDataFiles.subscriptionsFile);
-      const subscriptions1b = retainCorrectSubscriptions(subscriptions1a)
+      const subscriptions1 = subscriptionsDataFileToSubscriptions(zuoraDataFiles.subscriptionsFile);
+      const subscriptions2 = retainCorrectSubscriptions(subscriptions1)
       const holidaySubscriptionNames = holidayNamesDataFileToNames(zuoraDataFiles.holidayNamesFile);
       console.log(holidaySubscriptionNames);
-      const subscriptions2 = excludeHolidaySubscriptions(subscriptions1b, holidaySubscriptionNames);
+      const subscriptions3 = excludeHolidaySubscriptions(subscriptions2, holidaySubscriptionNames);
       const sentDate = now.format("DD/MM/YYYY");
       const deliveryDate = cursor.format("DD/MM/YYYY");
-      const fileRecords = subscriptionsToFileRecords(subscriptions2, sentDate, deliveryDate);
+      const fileRecords = subscriptionsToFileRecords(subscriptions3, sentDate, deliveryDate);
       const file2 = fileRecordsToCSVFile(fileRecords);
       const filePathKey = `fulfilment/${cursor.format("YYYY")}/${cursor.format("YYYY-MM")}/${cursor.format("YYYY-MM-DD")}.csv`;
       await commitFileToS3_v3(Stage, filePathKey, file2);
