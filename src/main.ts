0ccd4983eba51cf0b8b060b3ac0c0695af84c772
Pascal Honoré <pascal@alseyn.net>
2023-10-27 20:51:58 +0100

    refactoring

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,28 @@ import { Credentials } from 'aws-sdk/lib/core';
 import { getSsmValue } from "./utils/ssm";
 import { sleep } from "./utils/sleep";
 
+export const main = async () => {
+  console.log("main function: start");
+  const now = moment();
+  const zuoraBearerToken = await fetchZuoraBearerToken2(Stage);
+  if (zuoraBearerToken) {
+    await generateFilesForAllDays(zuoraBearerToken, now);
+  } else {
+    console.log("Could not extract a bearer token from zuora")
+  }
+  console.log("main function: completed");
+};
+
+async function generateFilesForAllDays(zuoraBearerToken: string, now: moment.Moment) {
+  const today = now.format("YYYY-MM-DD");
+  const indices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]; // There probably a sexier way to do this
+  const promises = indices.map(async (i) => {
+    const cursor = moment().add(i, "days");
+    return generateFileForDay(zuoraBearerToken, now, cursor);
+  });
+  await Promise.all(promises);
+}
+
 async function generateFileForDay(zuoraBearerToken: string, now: moment.Moment, cursor: moment.Moment) {
   const today = now.format("YYYY-MM-DD");
   const date = cursor.format("YYYY-MM-DD");
@@ -26,25 +48,3 @@ async function generateFileForDay(zuoraBearerToken: string, now: moment.Moment, 
   const filePathKey = `fulfilment/${cursor.format("YYYY")}/${cursor.format("YYYY-MM")}/${cursor.format("YYYY-MM-DD")}.csv`;
   await commitFileToS3_v3(Stage, filePathKey, file2);
 }
-
-async function generateFilesForAllDays(zuoraBearerToken: string, now: moment.Moment) {
-  const today = now.format("YYYY-MM-DD");
-  const indices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]; // There probably a sexier way to do this
-  const promises = indices.map(async (i) => {
-    const cursor = moment().add(i, "days");
-    return generateFileForDay(zuoraBearerToken, now, cursor);
-  });
-  await Promise.all(promises);
-}
-
-export const main = async () => {
-  console.log("main function: start");
-  const now = moment();
-  const zuoraBearerToken = await fetchZuoraBearerToken2(Stage);
-  if (zuoraBearerToken) {
-    await generateFilesForAllDays(zuoraBearerToken, now);
-  } else {
-    console.log("Could not extract a bearer token from zuora")
-  }
-  console.log("main function: completed");
-};
