6dac7f9869f0881b2126308d3c6371eeb79a8e2f
Pascal Honoré <pascal@alseyn.net>
2023-10-27 13:19:37 +0100

    Introduce generateFileAtDay

--- src/main.ts
+++ src/main.ts
@@ -9,27 +9,30 @@ import { Credentials } from 'aws-sdk/lib/core';
 import { getSsmValue } from "./utils/ssm";
 import { sleep } from "./utils/sleep";
 
+async function generateFileAtDay(zuoraBearerToken: string, now: moment.Moment, cursor: moment.Moment) {
+  const today = now.format("YYYY-MM-DD");
+  const date = cursor.format("YYYY-MM-DD");
+  console.log(`date: ${date}`);
+  const zuoraDataFiles = await cycleDataFilesFromZuora(Stage, zuoraBearerToken, date, today);
+  const currentSubs = subscriptionsDataFileToSubscriptions(zuoraDataFiles.subscriptionsFile);
+  const subsWithoutInvalid = retainCorrectSubscriptions(currentSubs)
+  const holidaySubscriptionNames = holidayNamesDataFileToNames(zuoraDataFiles.holidayNamesFile);
+  console.log(holidaySubscriptionNames);
+  const subsWithoutHolidayStops = excludeHolidaySubscriptions(subsWithoutInvalid, holidaySubscriptionNames);
+  const sentDate = now.format("DD/MM/YYYY");
+  const deliveryDate = cursor.format("DD/MM/YYYY");
+  const fileRecords = subscriptionsToFileRecords(subsWithoutHolidayStops, sentDate, deliveryDate);
+  const file2 = fileRecordsToCSVFile(fileRecords);
+  const filePathKey = `fulfilment/${cursor.format("YYYY")}/${cursor.format("YYYY-MM")}/${cursor.format("YYYY-MM-DD")}.csv`;
+  await commitFileToS3_v3(Stage, filePathKey, file2);
+}
+
 async function generateFilesForAllDays(zuoraBearerToken: string, now: moment.Moment) {
   const today = now.format("YYYY-MM-DD");
   const indices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]; // There probably a sexier way to do this
   for (const i of indices) {
-    console.log(`i: ${i}`);
-    console.log(`i: ${i}; timestamp: ${new Date()}`);
     const cursor = moment().add(i, "days");
-    const date = cursor.format("YYYY-MM-DD");
-    console.log(`i: ${i}; date: ${date}`);
-    const zuoraDataFiles = await cycleDataFilesFromZuora(Stage, zuoraBearerToken, date, today);
-    const currentSubs = subscriptionsDataFileToSubscriptions(zuoraDataFiles.subscriptionsFile);
-    const subsWithoutInvalid = retainCorrectSubscriptions(currentSubs)
-    const holidaySubscriptionNames = holidayNamesDataFileToNames(zuoraDataFiles.holidayNamesFile);
-    console.log(holidaySubscriptionNames);
-    const subsWithoutHolidayStops = excludeHolidaySubscriptions(subsWithoutInvalid, holidaySubscriptionNames);
-    const sentDate = now.format("DD/MM/YYYY");
-    const deliveryDate = cursor.format("DD/MM/YYYY");
-    const fileRecords = subscriptionsToFileRecords(subsWithoutHolidayStops, sentDate, deliveryDate);
-    const file2 = fileRecordsToCSVFile(fileRecords);
-    const filePathKey = `fulfilment/${cursor.format("YYYY")}/${cursor.format("YYYY-MM")}/${cursor.format("YYYY-MM-DD")}.csv`;
-    await commitFileToS3_v3(Stage, filePathKey, file2);
+    await generateFileAtDay(zuoraBearerToken, now, cursor);
     await sleep(2000); // sleeping 2 seconds
   }
 }
