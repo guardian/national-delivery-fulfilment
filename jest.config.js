60c46a92e414bc0fcde2cbb31e161c89bf9a2576
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2023-11-10 11:30:51 +0000

    test: setup just with test coverage example

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ["cdk", "node_modules", "project", "target"]
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	modulePathIgnorePatterns: ['dist/', 'cdk/', 'node_modules/'],
+	collectCoverage: true,
+	collectCoverageFrom: ['src/**/*.ts', '!**/*.test.ts'],
+	coverageThreshold: {
+		global: {
+			branches: 0,
+			functions: 2,
+			lines: 5,
+			statements: 5,
+		},
+	},
 };
