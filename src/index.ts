89057e48322694aa1fbc6b364ca16c819f97f42d
Pascal Honoré <pascal@alseyn.net>
2025-03-20 14:09:41 +0000

    add execution alarm

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import { Handler } from 'aws-lambda';
 import { main } from './main';
 
 export const handler: Handler = async (event) => {
-    /*
+  /*
 
-  We receive two kinds of events, either the event sent by AWS during the scheduled run, which looks like this:
+    We receive two kinds of events, either the event sent by AWS during the scheduled run, which looks like this:
     {
         "version": "0",
         "id": "f53402cc-287b-663b-f734-31ea35f66df9",
@@ -27,5 +27,5 @@ export const handler: Handler = async (event) => {
 
   */
 
-    await main(event['indices']);
+  await main(event['indices']);
 };
