ee686858567646eab04cb07ceeae16a427f4c3fa
Rupert Bates <Rupert.Bates@guardian.co.uk>
2025-05-15 12:45:44 +0100

    Fix parse-path error

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,9 +1,10 @@
 import "source-map-support/register";
 // https://github.com/guardian/cdk/blob/main/src/experimental/riff-raff-yaml-file/README.md 
-import { GuRootExperimental } from "@guardian/cdk/lib/experimental/constructs/root";
-import { NationalDeliveryFulfilment } from "../lib/national-delivery-fulfilment";
 
-const app = new GuRootExperimental();
+import { NationalDeliveryFulfilment } from "../lib/national-delivery-fulfilment";
+import { App } from 'aws-cdk-lib';
+
+const app = new App();
 new NationalDeliveryFulfilment(app, "NationalDeliveryFulfilment-CODE", { 
     env: { region: 'eu-west-1' },
     stack: "membership", 
